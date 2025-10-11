374786c98a3bb52a9e2aa8e6423631dcb7e9d773
Hector <hector@gmail.com>
2025-10-11 16:47:21 -0500

    mi puntito para T2

---
+++ src/main/java/com/puntos/ProyectoNavarroApplication.java
@@ -0,0 +1,13 @@
+package com.puntos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoNavarroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoNavarroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/puntos/ProyectoNavarroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puntos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoNavarroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
